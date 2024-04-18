9d3b02caf9c00b0a9191bbd066af08c9d8c32f29
Aliaskar Urakov <urakov.aliaskar@gmail.com>
2024-04-18 16:43:15 -0700

    initial commit

---
+++ src/components/Balance.vue
@@ -0,0 +1,6 @@
+<template>
+  <div>
+    <h4>Your Balance</h4>
+    <h1 id="balance">$0.00</h1>
+  </div>
+</template>
---
+++ src/components/Header.vue
@@ -0,0 +1,10 @@
+<template>
+  <h2>Expense Tracker</h2>
+</template>
+
+<script>
+
+</script>
+
+<style scoped>
+</style>
